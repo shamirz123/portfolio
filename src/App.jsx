bb777e95db3130a6fac9970a3c0cdcfb091c2640
Raja Shamir <153479520+shamirz123@users.noreply.github.com>
2024-02-01 11:22:04 +0500

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Components/Home/Home';
 import ReactWhatsapp from 'react-whatsapp';
+import ParticlesComponent from './js/partical';
 
 function App() {
   return (
     <Router>
-      <div>
-      <ReactWhatsapp number="+92 3115386005" message="Hello World!!!" />
+      <div >
+        <ParticlesComponent/>
+        <ReactWhatsapp number="+92 3115386005" message="Hello World!!!" />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
