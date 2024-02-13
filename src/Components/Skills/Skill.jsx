177d2394077e4e75ffc30db49a74e2abb02f6e1f
Raja Shamir <153479520+shamirz123@users.noreply.github.com>
2024-02-13 18:18:03 +0500

    ..

--- src/Components/Skills/Skill.jsx
+++ src/Components/Skills/Skill.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import { FaAsterisk } from "react-icons/fa";
+
 
 function Skill() {
     return (
@@ -6,91 +8,105 @@ function Skill() {
             <div className="container">
                 <div className="skill-wrapper">
                     <div className="row">
-                        <div className="col-lg-4 col-md-8 col-sm-12 mt-2">
-                            <h4>My Skills</h4>
+                        <div className="col-lg-6 col-md-8 col-sm-6 mt-2">
+                            {/* <h4>My Skills</h4>
                             <h1 className='m-0'>Let’s Explore Popular <br /><span>Skills & Experience</span></h1>
-                            <p>With a solid foundation in languages like JavaScript and proficiency in frameworks such as React and Node.js, I've honed my skills to create dynamic and responsive web solutions. Over the past three years, I've successfully completed over 50 projects, gaining invaluable experience and a keen eye for detail.</p>
+                            <p>With a solid foundation in languages like JavaScript and proficiency in frameworks such as React and Node.js, I've honed my skills to create dynamic and responsive web solutions. Over the past three years, I've successfully completed over 50 projects, gaining invaluable experience and a keen eye for detail.</p> */}
+                            <h5 > <i><FaAsterisk /></i> professional skill of me</h5>
+                            <h1>professional skill</h1>
+
+                            {/* <img className='' src="/assets/img/skill-man.png" alt="" /> */}
+
+                            <div class="d-flex p-3 skills-arrow-img-section">
+                                <div class="">
+                                    <img className='' src="/assets/img/skill-man.png" alt="" />
+                                </div>
+                                <div class="">
+                                    <img className='d-inline' src="/assets/img/skill-arrow.png" alt="" />
+                                </div>
+                            </div>
                         </div>
-                        <div className="col-lg-8 col-sm-12 col-md-12">
+                        <div className="col-lg-6 col-sm-6 col-md-12">
                             <div className="row">
-                                <div className="col-lg-3 col-sm-6 col-md-6">
+                                <div className="col-lg-4 col-sm-6 col-md-6">
                                     <div className="language-section">
                                         <div className='d-flex justify-content-center'>
                                             <img src="/assets/img/html.png" alt="" />
                                         </div>
-                                        <p className='text-center text-white fw-bold mt-2'>HTML</p>
+                                        {/* <p className='text-center text-white fw-bold mt-2'>HTML</p> */}
                                         <button>97%</button>
                                     </div>
                                 </div>
-                                <div className="col-lg-3 col-sm-6 col-md-6">
+                                <div className="col-lg-4 col-sm-6 col-md-6">
                                     <div className="language-section">
                                         <div className='d-flex justify-content-center'>
                                             <img src="/assets/img/css.png" alt="" />
                                         </div>
-                                        <p className='text-center text-white fw-bold mt-2'>CSS</p>
+                                        {/* <p className='text-center text-white fw-bold mt-2'>CSS</p> */}
                                         <button>88%</button>
                                     </div>
                                 </div>
-                                <div className="col-lg-3 col-sm-6 col-md-6">
+                                <div className="col-lg-4 col-sm-6 col-md-6">
                                     <div className="language-section">
                                         <div className='d-flex justify-content-center'>
                                             <img src="/assets/img/bootstrap.png" alt="" />
                                         </div>
-                                        <p className='text-center text-white fw-bold mt-0'>BOOTSTRAP</p>
-                                        <button>85%</button>
+                                        {/* <p className='text-center text-white fw-bold mt-0'>BOOTSTRAP</p> */}
+                                        <button className='m-0 pl-0'>85%</button>
                                     </div>
                                 </div>
-                                <div className="col-lg-3 col-sm-6 col-md-6">
-                                    <div className="language-section">
+                                <div className="col-lg-4 col-sm-6 col-md-6">
+                                    <div className="language-section mt-3">
                                         <div className='d-flex justify-content-center'>
-                                            <img src="/assets/img/js.png"   alt="" />
+                                            <img src="/assets/img/js.png" alt="" />
                                         </div>
-                                        <p className='text-center text-white fw-bold mt-4 fs-5'>JS</p>
+                                        {/* <p className='text-center text-white fw-bold mt-4 fs-5'>JS</p> */}
                                         <button>80%</button>
                                     </div>
                                 </div>
-                                <div className="col-lg-3 col-sm-6 col-md-6">
+                                <div className="col-lg-4 col-sm-6 col-md-6">
                                     <div className="language-section mt-3">
                                         <div className='d-flex justify-content-center'>
                                             <img src="/assets/img/jquery.png" alt="" />
                                         </div>
-                                        <p className='text-center text-white fw-bold mt-2'>JQUERY</p>
+                                        {/* <p className='text-center text-white fw-bold mt-2'>JQUERY</p> */}
                                         <button>80%</button>
                                     </div>
                                 </div>
-                                <div className="col-lg-3 col-sm-6 col-md-6 ">
+                                <div className="col-lg-4 col-sm-6 col-md-6 ">
                                     <div className="language-section mt-3">
                                         <div className='d-flex justify-content-center'>
                                             <img src="/assets/img/react.png" alt="" />
                                         </div>
-                                        <p className='text-center text-white fw-bold mt-2'>REACT</p>
+                                        {/* <p className='text-center text-white fw-bold mt-2'>REACT</p> */}
                                         <button>90%</button>
                                     </div>
                                 </div>
-                                <div className="col-lg-3 col-sm-6 col-md-6">
+                                <div className="col-lg-4 col-sm-6 col-md-6">
                                     <div className="language-section mt-3">
                                         <div className='d-flex justify-content-center'>
-                                            <img src="/assets/img/php.png" style={{width:'67px'}}  alt="" />
+                                            <img src="/assets/img/php.png" style={{ width: '67px' }} alt="" />
                                         </div>
-                                        <p className='text-center text-white fw-bold mt-2'>PHP</p>
+                                        {/* <p className='text-center text-white fw-bold mt-2'>PHP</p> */}
                                         <button>70%</button>
                                     </div>
                                 </div>
-                                <div className="col-lg-3 col-sm-12 col-md-6">
+                                <div className="col-lg-4 col-sm-12 col-md-6">
                                     <div className="language-section mt-3">
                                         <div className='d-flex justify-content-center'>
-                                            <img className='figma-img' style={{width:'32px'}} src="/assets/img/figma.png" alt="" />
+                                            <img className='figma-img' style={{ width: '32px' }} src="/assets/img/figma.png" alt="" />
                                         </div>
-                                        <p className='text-center text-white fw-bold mt-0'>FIGMA</p>
+                                        {/* <p className='text-center text-white fw-bold mt-0'>FIGMA</p> */}
                                         <button>67%</button>
                                     </div>
                                 </div>
-                           
+
                             </div>
-                           
+
 
                         </div>
                     </div>
+                    
                 </div>
             </div>
         </>
